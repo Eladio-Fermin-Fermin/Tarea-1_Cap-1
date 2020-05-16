c367ac6586c84f23511357e8a65864e3ff488466
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-05-16 10:36:43 -0400

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
